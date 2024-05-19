ac4c4d8de252de8b7555ee3ed339147ae9a2dec7
Pedro Sousa <pompermayerpedrohenrique@gmail.com>
2024-05-18 22:40:49 -0300

    Criação do projeto com Vue / criação da tela de Login

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <NavBarComponents />
+    <DashBoard />
+    <!--<LoginForm />-->
+    <FooterComponents />
+  </div>
+  
+</template>
+
+<script>
+  //import LoginForm from './views/LoginForm.vue';
+  import DashBoard from './views/DashBoard.vue';
+  import NavBarComponents from './components/NavBarComponents.vue';
+  import FooterComponents from './components/FooterComponents.vue';
+  
+
+  
+
+  export default {
+    name: 'App',
+    components: {
+      NavBarComponents,
+      DashBoard,
+      //LoginForm,
+      FooterComponents
+    }
+  }
+</script>
---
+++ src/views/LoginForm.vue
@@ -0,0 +1,118 @@
+<template>
+  <body>
+    <div class="container container-signin">
+      <section class="sec-login">
+        <form class="form-login">
+          <h1>ENTRAR</h1>
+          <div>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Usuário"
+            /><br />
+            <input type="password" id="pwd" name="pwd" placeholder="Senha" />
+          </div>
+          <input class="btn-default" type="button" value="Acessar" @click="login"/>
+        </form>
+      </section>
+    </div>
+  </body>
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+
+export default {
+  name: 'LoginForm',
+  setup() {
+    const router = useRouter();
+
+    const login = () => {
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('pwd').value;
+
+      if (username === 'admin' && password === 'admin') {
+        router.push('./components/DashBoard'); // Navega para a rota desejada
+      } else {
+        alert('Usuário ou senha incorretos');
+      }
+    };
+
+    return { login };
+  },
+};
+</script>
+
+<style>
+      /* Layout Geral */
+  body{
+      margin: 0;
+      padding: 0;
+  }
+
+  .container{
+      width: 100%;
+      height: 100vh;
+      background: #FCF9D8;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+
+  .btn-default{
+      cursor: pointer;
+      background-color: #000;
+      width: 310px;
+      height: 35px;
+      padding: 0px;
+      padding-right: 10px;
+      color: white;
+      text-align: right;
+      font-size: x-large;
+  }
+  .btn-default:hover{
+      background-color: #ffffff;
+      color: #000;
+      border: #0000005e solid 1px;
+  }
+
+  h1{
+      font-family: "Poiret One", sans-serif;
+      font-size: 40px;
+      color: white;
+  }
+
+  /* Page Login */
+  .container-signin{
+      background-image: url(../assets/33\ 1.png);
+      background-repeat: no-repeat;
+      background-size: cover;
+      font-family: "Poiret One", sans-serif;
+  }
+
+  .sec-login {
+      background-color: rgba(9, 166, 163, 1);
+      width: 300px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 30px;
+      margin-right: 500px;
+      padding: 30px;
+      font-family: "Poiret One", sans-serif;
+      text-align: center;
+  }
+
+  input {
+      background-color: rgba(237, 235, 201, 1);
+      margin-bottom: 30px;
+      padding: 5px;
+      width: 300px;
+      height: 35px;
+      border: none;
+      font-family: "Poiret One", sans-serif;
+      color: #000000;
+      font-size: large;
+  }
+</style>
