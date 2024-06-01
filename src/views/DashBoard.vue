754c6d34a101e3bce162e037c7a8eac8f579b27a
Pedro Sousa <pompermayerpedrohenrique@gmail.com>
2024-05-31 22:44:13 -0300

    alteração da tela de dashboard, novos campos e mudanças nos componentes por conta do novo css

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,20 +1,38 @@
 <template>
-    <body>
-        <main>
-            <section class="Dash">
-                <div class="container-dash">
-                    <div class="flex-dash">
-                        <div class="head-dash"> DashBoard
-                            <button class="btn-action-dash">+</button>
-                            <div class="input-dash">
-                                <input type="text" class="input-pesquisa-dash" placeholder="Pesquisar Tcc">
-                            </div>
-                        </div>
-                    </div>
+        <div class="container">    
+            <div class="workspace ">
+                <div class="head">DASHBOARD
+                    <button class="btn-action">+</button>
                 </div>
-            </section>
-        </main>
-    </body>
+                <div class="search">
+                <input class="input-search" type="search" placeholder="Buscar dados">
+                    <input class="btn-default btn-search" type="button" value="Buscar">
+                </div>
+                <table >
+                    <tr class="table-title">
+                        <td>Nome</td>
+                        <td>Idade</td>
+                        <td>Profissão</td>
+                    </tr>
+                    <tr>
+                        <td>Ted</td>
+                        <td>22</td>
+                        <td>Estudante</td>
+                    </tr>
+                    <tr>
+                        <td>Ralf</td>
+                        <td>26</td>
+                        <td>Designer</td>
+                    </tr>
+                    <tr>
+                        <td>Ralf</td>
+                        <td>26</td>
+                        <td>Designer</td>
+                    </tr>                 
+                </table>
+      
+            </div> 
+        </div>
 </template>
   
 <script>
@@ -23,78 +41,11 @@
     }
 </script>
 
-<style>
-    body{
-      margin: 0;
-      padding: 0;
-    }
+<style scoped>
 
-    .flex-dash{
-        display: flex;
-    }
+.btn-default{
+    width: 85px;
+}
 
-    .container-dash{
-        width: 100%;
-        height: 100vh;
-        background: #f8f8f8;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-
-    .flex-dash{
-        width:750px;
-        height: 450px;
-        background-color: white;
-        display: grid;
-        align-items: top;
-        justify-content: center;
-        box-shadow: 0 0 1000px 0  #09a6a38e;
-        display: flex;
-        border-style: solid;
-        border-color: #09a6a3;
-    }
-
-    .head-dash{
-        background-color: #09a6a3;
-        align-items: center;
-        width: 100%;
-        height: 35px;
-        font-size: 30px;
-        color: white; 
-        font-family: "Poiret One", sans-serif;
-        
-        text-align: center;
-    }
-
-    .btn-action-dash{
-        position: relative;
-        background-color: #ffffff;
-        border: #09a6a3 solid 1px;
-        border-radius: 100px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 35px;
-        height: 35px;
-        cursor: pointer;
-        font-size: xx-large;
-        color: #09a6a3;
-        left: 633px;
-        top: -19px;
-    }
-    .btn-action-dash:hover{
-        background-color: #09a6a3;
-        color: #ffffff;
-        border-style: solid;
-        border-color: #ffffff;
-        border-width: 2px; 
-    }
-
-    .input-pesquisa-dash{
-        width: 80%;
-        margin: 5px 0;
-    }
-    
-    
 </style>
+
