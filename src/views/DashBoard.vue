8d27bf564f47ac4acdfd697954a2037a8f2c96d8
Pedro Sousa <pompermayerpedrohenrique@gmail.com>
2024-06-08 12:56:01 -0300

    implementação da responsividade da tela de dashboard...

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -47,6 +47,67 @@
     width: 85px;
 }
 
+@media screen and (max-width: 630px) {
+   .container {
+        width: 100%;
+        max-width: 800px;
+    }
+
+   .search,.forms {
+        width: 400px; /* Ajusta para ocupar toda a largura disponível */
+    }
+
+    .btn-action {
+        left: 60px;
+    }
+}
+
+@media screen and (max-width: 430px) {
+   .container {
+        width: 100%;
+        max-width: 800px;
+    }
+
+   .search,.forms {
+        width: 300px; /* Ajusta para ocupar toda a largura disponível */
+    }
+
+    input.input-search {
+        font-family: "Poiret One", sans-serif;
+        display: flex;
+        align-items: top;
+        justify-content: center;
+        width: 180px;
+        margin: 4px;
+        margin-top: 2px;
+        font-size: 15px;
+    }
+
+    .btn-default {
+        cursor: pointer;
+        background-color: #000;
+        width: 60px;
+        height: 35px;
+        padding: 0px;
+        text-align: center;
+        font-size: 15px;
+    }
+
+    .btn-action {
+        width: 28px;
+        height: 28px;
+        font-size: 30px;
+        left: 30px;
+    }
+
+    .workspace{
+        margin-top: 90px;
+    }
+
+
+}
+
+
 
 </style>
 
