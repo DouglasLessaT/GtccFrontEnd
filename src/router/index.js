75d05be785306134aaa440075761b9d6e6c38cea
Pedro Sousa <pompermayerpedrohenrique@gmail.com>
2024-06-11 18:05:54 -0300

    Acertos da rota de login e dashboard, junto com a verificação do token para acesso a tela de dash

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,25 @@ import CadastroAluno from '../views/CadastroAluno.vue';
 import CadastroBanca from '../views/CadastroBanca.vue';
 import ConfirmaRelatorio from '../views/ConfirmaRelatorio.vue';
 
+import LoginService from '../services/LoginService'
+import AuthService from '@/services/AuthService'
+
 const routes = [
     { 
-    path: "/", 
+    path: "/login", 
+    name: 'login',
     component: LoginForm
     },
 
+    //{
+    //path: '/nao-autorizado',
+    //name: 'nao-autorizado',
+    //component: NaoAutorizadoView
+    //},
+
     {
-    path: "/dashboard",
+    path: "/",
+    name: 'dashboard',
     component: DashBoard
     },
    
@@ -40,7 +51,25 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(),
-    routes,
-});
+    routes
+})
+
+router.beforeEach((to, from) => {
+    LoginService.router = router;
+  
+    console.log(`de ${from.name} para ${to.name}`);
+  
+  
+    if (!LoginService.estaAutenticado() && to.name != 'login') {
+      return { name: 'login' }
+    }
+  
+    if (!AuthService.validaPermissao(to.path)) {
+      router.push('/nao-autorizado')
+      return false;
+    }
+  
+    return true;
+  })
 
 export default router;
