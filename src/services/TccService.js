09dbb99b649642c18326bfff2785ebb1a8189581
Hugo <113729299+hugo-exe@users.noreply.github.com>
2024-06-19 21:45:13 -0300

    alterações e funcionalidade no select

--- src/services/TccService.js
+++ src/services/TccService.js
@@ -1,13 +1,39 @@
 // services/TccService.js
-import axios from 'axios';
+import axios from "axios";
+import AuthService from "./AuthService";
 
-const baseURL = "http://localhost:8080/gtcc/coordenacao/tcc/v1";
+
+const baseURL = "http://localhost:8083/gtcc/coordenacao/tcc/v1/tcc";
+const baseURLGetAlunos =
+  "http://localhost:8083/gtcc/coordenacao/tcc/v1/Professor/alunos";
 
 class TccService {
+  parser(obj) {
+    return {
+      curse: obj.curso,
+      idAluno: obj.aluno,
+      idOrientador: obj.orientador,
+      theme: obj.tema,
+      title: obj.titulo,
+    };
+  }
+
   async cadastrarTCC(tcc) {
+    console.log("Teste I", tcc);
+    let tccAfterParse = this.parser(tcc);
+    console.log("Teste II", tccAfterParse);
     try {
-      const url = `${baseURL}/tcc`;
-      const response = await axios.post(url, tcc);
+      const url = `${baseURL}`;
+      //const response = await axios.post(url, tcc);
+      const response = await axios(url, {
+        method: "POST",
+        data: JSON.stringify(tccAfterParse),
+        headers: {
+          Authorization:
+            `Bearer ${AuthService.dados.token}`,
+          "Content-Type": "application/json",
+        },
+      });
       return response.data;
     } catch (error) {
       throw new Error(`Erro ao cadastrar TCC: ${error.message}`);
@@ -16,13 +42,32 @@ class TccService {
 
   async buscarAlunos() {
     try {
-      const url = `${baseURL}/usuarios`;
+      const url = `${baseURLGetAlunos}`;
       const response = await axios.get(url);
       return response.data;
     } catch (error) {
       throw new Error(`Erro ao buscar alunos: ${error.message}`);
     }
   }
+
+  buscarAlunos2() {
+    try {
+      const url = `${baseURLGetAlunos}`;
+      return axios.get(url);
+    } catch (error) {
+      throw new Error(`Erro ao buscar alunos: ${error.message}`);
+    }
+  }
+
+  buscarProfessor() {
+    try {
+      const url = `${baseURLGetAlunos}`;
+      return axios.get(url);
+    } catch (error) {
+      throw new Error(`Erro ao buscar alunos: ${error.message}`);
+    }
+  }
+
 }
 
 export default new TccService();
--- src/views/CadastroTcc.vue
+++ src/views/CadastroTcc.vue
@@ -1,110 +1,132 @@
 <template>
-  <div class="container">
-    <div class="workspace">
-      <div class="head">
-        Cadastro de TCC
-        <button class="btn-action">></button>
-      </div>
-      <div class="forms">
-        <form @submit.prevent="submitForm" class="form-cadtcc">
-          <div class="input-container-cadastrotcc">
-            <input
-              type="text"
-              class="input-preencher2-tcc"
-              placeholder="Titulo do TCC"
-              v-model="tcc.titulo"
-            />
-            <input
-              type="text"
-              class="input-preencher2-tcc"
-              placeholder="Curso"
-              v-model="tcc.curso"
-            />
-            <input
-              type="text"
-              class="input-preencher3-tcc"
-              placeholder="Tema"
-              v-model="tcc.tema"
-            />
-            <select class="select-preencher4-tcc" v-model="tcc.aluno">
-              <option value="">Selecione um aluno</option>
-              <option
-                v-for="aluno in alunos"
-                :key="aluno.id"
-                :value="aluno.id"
-              >
-                {{ aluno.nome }}
-              </option>
-            </select>
-            <input
-              type="number"
-              class="input-preencher1-tcc"
-              placeholder="Número de TCC"
-              v-model="tcc.numero"
-            />
-          </div>
-          <button type="submit" class="btn-submit">Enviar</button>
-        </form>
-      </div>
-    </div>
+ <div class="container">
+  <div class="workspace">
+   <div class="head">
+    Cadastro de TCC
+    <button class="btn-action">></button>
+   </div>
+   <div class="forms">
+    <form @submit.prevent="submitForm" class="form-cadtcc">
+     <div class="input-container-cadastrotcc">
+      <input
+       type="text"
+       class="input-preencher2-tcc"
+       placeholder="Titulo do TCC"
+       v-model="tcc.titulo"
+      />
+      <input
+       type="text"
+       class="input-preencher2-tcc"
+       placeholder="Curso"
+       v-model="tcc.curso"
+      />
+      <input
+       type="text"
+       class="input-preencher3-tcc"
+       placeholder="Tema"
+       v-model="tcc.tema"
+      />
+      <select class="select-preencher4-tcc" v-model="tcc.aluno">
+       <option value="">Selecione um aluno</option>
+       <option v-for="aluno in alunos" :key="aluno.id" :value="aluno.id">
+        {{ aluno.name }}
+       </option>
+      </select>
+      <input
+       type="number"
+       class="input-preencher1-tcc"
+       placeholder="Número de TCC"
+       v-model="tcc.numero"
+      />
+     </div>
+     <button type="submit" class="btn-submit">Enviar</button>
+    </form>
+   </div>
   </div>
+ </div>
 </template>
 
 <script>
 import TccService from "@/services/TccService";
 
 export default {
-  name: "CadastroTcc",
-  data() {
-    return {
-      tcc: {
-        numero: "",
-        titulo: "",
-        curso: "",
-        tema: "",
-        aluno: "",
-      },
-      alunos: [],
-    };
+ name: "CadastroTcc",
+ data() {
+  return {
+   tcc: {
+    numero: "",
+    titulo: "",
+    curso: "",
+    tema: "",
+    aluno: "",
+   },
+   alunos: [],
+  };
+ },
+ mounted() {
+  //console.log("Componente CadastroTcc montado.");
+  this.carregarAlunos();
+ },
+ methods: {
+  carregarAlunos() {
+   let self = this;
+   try {
+    console.log("Buscando alunos...");
+    /*let response = await TccService.buscarAlunos();
+    console.log(response);
+    this.alunos = response;
+    console.log(this.alunos);*/
+
+    TccService.buscarAlunos2().then((a) => {
+     self.alunos = a.data;
+    });
+   } catch (error) {
+    console.error("Erro ao buscar alunos:", error);
+    alert("Erro ao carregar a lista de alunos");
+   }
   },
-  async mounted() {
-    console.log("Componente CadastroTcc montado.");
-    this.carregarAlunos();
+
+  carregarProfessor() {
+   let self = this;
+   try {
+    console.log("Buscando professor...");
+    /*let response = await TccService.buscarAlunos();
+    console.log(response);
+    this.alunos = response;
+    console.log(this.alunos);*/
+
+    TccService.buscarAlunos2().then((a) => {
+     self.alunos = a.data;
+    });
+   } catch (error) {
+    console.error("Erro ao buscar alunos:", error);
+    alert("Erro ao carregar a lista de alunos");
+   }
   },
-  methods: {
-    async carregarAlunos() {
-      try {
-        console.log("Buscando alunos...");
-        const response = await TccService.buscarAlunos();
-        this.alunos = response; // Verifique se response é um array esperado
-        console.log("Alunos carregados:", this.alunos);
-      } catch (error) {
-        console.error("Erro ao buscar alunos:", error);
-        alert("Erro ao carregar a lista de alunos");
-      }
-    },
-    async submitForm() {
-      try {
-        console.log("Dados do formulário antes de enviar:", this.tcc);
-        const response = await TccService.cadastrarTCC(this.tcc);
-        console.log("Resposta do backend:", response);
-        alert("Cadastro realizado com sucesso!");
-        this.resetarFormulario();
-      } catch (error) {
-        console.error("Erro ao enviar formulário:", error);
-        alert("Erro ao cadastrar TCC");
-      }
-    },
-    resetarFormulario() {
-      this.tcc = {
-        numero: "",
-        titulo: "",
-        curso: "",
-        tema: "",
-        aluno: "",
-      };
-    },
+
+  async submitForm() {
+   try {
+    console.log("Dados do formulário antes de enviar:", this.tcc);
+    const response = await TccService.cadastrarTCC(this.tcc);
+    console.log("Resposta do backend:", response);
+    alert("Cadastro realizado com sucesso!");
+    this.resetarFormulario();
+   } catch (error) {
+    console.error("Erro ao enviar formulário:", error);
+    alert("Erro ao cadastrar TCC");
+   }
   },
+  resetarFormulario() {
+   this.tcc = {
+    numero: "",
+    titulo: "",
+    curso: "",
+    tema: "",
+    aluno: "",
+    orientador: "",
+   };
+  },
+ },
 };
 </script>
 
@@ -114,69 +136,69 @@ export default {
 .input-preencher2-tcc,
 .input-preencher3-tcc,
 .input-preencher4-tcc {
-  width: 80%;
-  margin: 10px 0;
+ width: 80%;
+ margin: 10px 0;
 }
 
 .select-preencher4-tcc {
-  border-radius: 2px;
-  width: 82%;
-  margin: 10px 0;
-  height: 40px;
-  background-color: rgba(237, 235, 201, 1);
+ border-radius: 2px;
+ width: 82%;
+ margin: 10px 0;
+ height: 40px;
+ background-color: rgba(237, 235, 201, 1);
 }
 
 @media screen and (max-width: 600px) {
-  .forms {
-    margin-left: 12px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    margin-top: 5px;
-  }
+ .forms {
+  margin-left: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 5px;
+ }
 
-  .container {
-    width: 100%;
-    margin-top: 20%;
-    padding: 10px;
-    box-sizing: border-box;
-  }
+ .container {
+  width: 100%;
+  margin-top: 20%;
+  padding: 10px;
+  box-sizing: border-box;
+ }
 
-  .head {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px;
-    box-sizing: border-box;
-    background-color: #009688;
-    color: white;
-    position: relative;
-    height: 80px;
-  }
+ .head {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: #009688;
+  color: white;
+  position: relative;
+  height: 80px;
+ }
 
-  .btn-action {
-    background-color: #ffffff;
-    border: #09a6a3 solid 1px;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 35px;
-    height: 35px;
-    cursor: pointer;
-    font-size: large;
-    color: #09a6a3;
-    margin-top: 35px;
-    position: relative;
-    margin-right: 100px;
-  }
+ .btn-action {
+  background-color: #ffffff;
+  border: #09a6a3 solid 1px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+  font-size: large;
+  color: #09a6a3;
+  margin-top: 35px;
+  position: relative;
+  margin-right: 100px;
+ }
 
-  .btn-action:hover {
-    background-color: #09a6a3;
-    color: #ffffff;
-    border: 2px solid #ffffff;
-  }
+ .btn-action:hover {
+  background-color: #09a6a3;
+  color: #ffffff;
+  border: 2px solid #ffffff;
+ }
 }
 </style>
