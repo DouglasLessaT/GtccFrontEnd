2e86b81bcc38049560a8a1eb6b2d57ff5830431d
SammLopes <sam.v.p.lopes@gmail.com>
2024-06-29 15:13:39 -0300

    Adicionado metodo para buscar de informações de tcc no back-end

--- src/services/DashService.js
+++ src/services/DashService.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import AuthService from "./AuthService";
 
-const urlBuscaLIstaDeAluno = "http://localhost:8083/gtcc/coordenacao/tcc/v1/tccs"
+const baseURL = "http://localhost:8083/gtcc/coordenacao/tcc/v1/tcc";
+const urlBuscaLIstaDeTccs = "http://localhost:8083/gtcc/coordenacao/tcc/v1/tccs"
 
 class DashService{
-    async buscarTCC() {
+    
+    async buscarTCCs() {
         try {
-            const response = await axios.get(urlBuscaLIstaDeAluno, {
+            const response = await axios.get(urlBuscaLIstaDeTccs, {
                 headers: {
                     Authorization: `Bearer ${AuthService.dados.token}`,
                     "Content-Type": "application/json",
@@ -14,8 +16,29 @@ class DashService{
             });
             return response.data;
         } catch (error) {
-            throw new Error(`Erro ao buscar usuários: ${error.message}`);
+            throw new Error(`Erro ao buscar tccs: ${error.message}`);
           }
+    }
+
+    async buscarTcc(title) {
+
+        if(title == null){
+            console.log('NUlo');
+        }
+        let titleNonSpaces = title.trim();
+        console.log(titleNonSpaces);
+        try {
+          const url = `${baseURL}/?title=${encodeURIComponent(titleNonSpaces)}`;
+          console.log(url);
+          const response = await axios.get(url, {
+            headers: {
+              Authorization: `Bearer ${AuthService.dados.token}`,
+            },
+          });
+          return response.data;
+        } catch (error) {
+          throw new Error(`Erro ao buscar o tcc: ${error.message}`);
+        }
     }
 
 }
